4e32fe057868ed51833ef94523b1b3642023914f
Htiango <hongty106@gmail.com>
2016-09-02 20:00:14 +0800

    Initial Commit

---
+++ SpeechRecognition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpeechRecognition
+//
+//  Created by hty on 9/2/16.
+//  Copyright © 2016 hty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
